ef4877277a2e1c382ecc1434715b48fee918c6f1
Sergey Ponomarev <stokito@gmail.com>
2023-09-27 12:03:40 +0300

    init

---
+++ downloader.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"errors"
+	"io"
+	"net/http"
+	"strconv"
+	"sync"
+	"sync/atomic"
+)
+
+type Processor interface {
+	ProcessChunk(chunk []byte)
+}
+
+type Downloader struct {
+	ctx       context.Context
+	Processor Processor
+	Url       string
+	// ProgressCh receives progress of download of the file in percents
+	ProgressCh chan int
+	// ChunkSize in bytes
+	ChunkSize      int
+	totalProcessed atomic.Int64
+	totalBytes     int64
+}
+
+func NewDownloader(ctx context.Context, processor Processor, url string, progressCh chan int, chunkSize int) *Downloader {
+	return &Downloader{
+		ctx:        ctx,
+		Processor:  processor,
+		Url:        url,
+		ProgressCh: progressCh,
+		ChunkSize:  chunkSize,
+	}
+}
+
+func (d *Downloader) Download() error {
+	req, err := http.NewRequestWithContext(d.ctx, "GET", d.Url, nil)
+	if err != nil {
+		return err
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	if resp.Body != nil {
+		defer resp.Body.Close()
+	}
+	if resp.StatusCode != http.StatusOK {
+		return errors.New("Incorrect status code: " + strconv.Itoa(resp.StatusCode))
+	}
+	d.totalBytes = resp.ContentLength
+	err = d.processResponse(resp.Body)
+	return err
+}
+
+func (d *Downloader) processResponse(repBody io.ReadCloser) error {
+	reader := bufio.NewReader(repBody)
+	wg := &sync.WaitGroup{}
+	err := d.readAndProcess(reader, wg)
+	wg.Wait()
+	close(d.ProgressCh)
+	// Explicitly close body
+	_ = repBody.Close()
+	return err
+}
+
+func (d *Downloader) readAndProcess(reader *bufio.Reader, wg *sync.WaitGroup) error {
+	for {
+		select {
+		case <-d.ctx.Done():
+			return d.ctx.Err()
+		default:
+			chunk, err := d.readChunk(reader)
+			if err != nil {
+				return err
+			}
+			if chunk == nil {
+				return nil
+			}
+			wg.Add(1)
+			go d.processChunk(chunk, wg)
+		}
+	}
+}
+
+func (d *Downloader) readChunk(reader *bufio.Reader) ([]byte, error) {
+	chunk := make([]byte, d.ChunkSize) //TODO use pool
+	readCount, err := reader.Read(chunk)
+	if err != nil && err != io.EOF {
+		return nil, err
+	}
+	if readCount == 0 {
+		return nil, nil
+	}
+	chunk = chunk[:readCount]
+	return chunk, nil
+}
+
+func (d *Downloader) processChunk(chunk []byte, wg *sync.WaitGroup) {
+	defer wg.Done()
+	if d.Processor != nil {
+		d.Processor.ProcessChunk(chunk)
+	}
+	totalProcessed := d.totalProcessed.Add(int64(len(chunk)))
+	progressPercent := totalProcessed * 100 / d.totalBytes
+	d.ProgressCh <- int(progressPercent)
+}
---
+++ downloader_test.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"strings"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestDownloader_Download(t *testing.T) {
+	d, progressCh := createTestDownloader()
+
+	err := d.Download()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	progress := make([]int, 0, 20)
+	for p := range progressCh {
+		progress = append(progress, p)
+		fmt.Printf("Downloaded: %d %%\n", p)
+	}
+	assert.EqualValues(t, []int{5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100}, progress)
+	assert.Equal(t, int64(200), d.totalBytes)
+}
+
+func TestDownloader_processChunk(t *testing.T) {
+	d, progressCh := createTestDownloader()
+	chunk := []byte("0123456789")
+	d.totalBytes = 200
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	d.processChunk(chunk, wg)
+	p := <-progressCh
+	assert.Equal(t, 5, p)
+}
+
+type PausedReader struct {
+	Reader io.Reader
+}
+
+func (r *PausedReader) Read(p []byte) (n int, err error) {
+	time.Sleep(4 * time.Second)
+	n, err = r.Reader.Read(p)
+	return
+}
+
+func TestDownloader_processChunk_cancel(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	url := "http://localhost:8081/test.txt"
+	progressCh := make(chan int)
+	chunkSize := 10
+	d := NewDownloader(ctx, nil, url, progressCh, chunkSize)
+
+	chunks := "0123456789" + "0123456789" + "0123456789"
+	d.totalBytes = 200
+	r := &PausedReader{strings.NewReader(chunks)}
+	body := io.NopCloser(r)
+	go d.processResponse(body)
+
+	p5 := <-progressCh
+	assert.Equal(t, 5, p5)
+	cancel()
+	p10 := <-progressCh
+	assert.Equal(t, 0, p10)
+	p15 := <-progressCh
+	assert.Equal(t, 0, p15)
+}
+
+type PrintingProcessor struct {
+}
+
+func (p *PrintingProcessor) ProcessChunk(chunk []byte) {
+	fmt.Printf("Process: %s\n", chunk)
+}
+
+func createTestDownloader() (*Downloader, chan int) {
+	url := "http://localhost:8081/test.txt"
+	progressCh := make(chan int, 20)
+	chunkSize := 10
+	ctx := context.Background()
+	processor := &PrintingProcessor{}
+	d := NewDownloader(ctx, processor, url, progressCh, chunkSize)
+	return d, progressCh
+}
