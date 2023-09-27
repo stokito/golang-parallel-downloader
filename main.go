90d76eb4990de48fb311e132d3005286111d089e
Sergey Ponomarev <stokito@gmail.com>
2023-09-27 15:04:56 +0300

    Add main

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	url, chunkSize := parseArgs()
+	progressCh := make(chan int, 200)
+	ctx := context.Background()
+	processor := &BufProcessor{}
+	d := NewDownloader(ctx, processor, url, progressCh, chunkSize)
+
+	go performDownload(d)
+
+	fmt.Printf("Download progress:\n")
+
+	for p := range progressCh {
+		fmt.Printf("%d %%\n", p)
+	}
+	fmt.Printf("%s", processor.Bytes())
+}
+
+func parseArgs() (string, int) {
+	if len(os.Args) == 1 {
+		_, _ = fmt.Fprintf(os.Stderr, "Usage downloader <file url> <chunk size>\n"+
+			"For example:\n"+
+			"downloader http://wttr.in/?A 200\n"+
+			"The chunk size may be omitted. Its default is 200 bytes\n")
+		os.Exit(1)
+	}
+	url := os.Args[1]
+	chunkSize := 200
+	if len(os.Args) >= 3 {
+		chunkSizeFromArg, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "Invalid chunk size: %s\n", err)
+			os.Exit(1)
+		}
+		chunkSize = chunkSizeFromArg
+	}
+	return url, chunkSize
+}
+
+func performDownload(d *Downloader) {
+	err := d.Download()
+	if err != nil {
+		fmt.Printf("Unable to download from %s: %s", d.Url, err)
+		os.Exit(2)
+		return
+	}
+}
--- processor.go
+++ processor.go
@@ -1,10 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"os"
+)
 
 type PrintingProcessor struct {
 }
 
 func (p *PrintingProcessor) ProcessChunk(chunk []byte) {
-	fmt.Printf("Process: %s\n", chunk)
+	fmt.Fprintf(os.Stderr, "%s", chunk)
+}
+
+type BufProcessor struct {
+	buf bytes.Buffer
+}
+
+func (p *BufProcessor) ProcessChunk(chunk []byte) {
+	_, _ = p.buf.Write(chunk)
+}
+
+func (p *BufProcessor) Bytes() []byte {
+	return p.buf.Bytes()
 }
